b617234f3338e4906ccf431b21085ce8836bf022
Andrei21A <andreianea29@gmail.com>
2025-03-04 18:59:19 +0200

    ex1 refacut

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 u = 1.0
 m = 0
 
@@ -5,4 +7,8 @@ while 1.0 + u != 1.0:
     m += 1
     u = 10 ** -m
 
-print(f"Cel mai mic u care satisface conditia este 10^(-{m-1})")
+u = 10 ** -(m-1)
+
+print(f"Cel mai mic u care satisface condiția este 10^(-{m-1}) = {u}")
+
+print(f"Precizia mașină din numpy: {np.finfo(float).eps}")
